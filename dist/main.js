cf2486dc97414263afbc7cb4ace8087673fa5f42
Uday vara <varaudayd@gmail.com>
2024-08-11 23:49:24 +0530

    --->Updated BUild

--- dist/main.js
+++ dist/main.js
@@ -23,7 +23,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);
-    await app.listen(5000);
+    await app.listen(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
